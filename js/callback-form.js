dc3a8b048b5e03ba3b9324288baf85773295f932
MartaLinux <akumakun507@gmail.com>
2022-08-02 15:03:24 +0300

    form-success

--- js/callback-form.js
+++ js/callback-form.js
@@ -3,6 +3,17 @@ const callbackRequestRecived = document.querySelector("#request-recived");
 const userName = document.querySelector("#inputName");
 const userEmail = document.querySelector("#email");
 const userPhone = document.querySelector("#inputPhone");
+//var ErrorModal = document.getElementById("#callback-form-input-error");
+var ErrorModal = new bootstrap.Modal(document.getElementById('callback-form-input-error'), {
+    keyboard: false
+
+  })
+
+
+var ModalSuccess = new bootstrap.Modal(document.getElementById('modalFormControlSuccess'), {
+    keyboard: false
+  })
+
 
 userPhone.addEventListener(
     'click',
@@ -21,23 +32,27 @@ userPhone.addEventListener(
     }
 );
 
-callbackFrom.addEventListener("submit", function(event){
+callbackFrom.addEventListener("submit", function(event) {
     event.preventDefault();
     
+    console.log("test")
     
     
-   /* let hasError =false;
+    
+    let hasError =false;
     if(!userName.value.trim()){
         userName.classList.add("callback-form-input-error");
         hasError = true;
+        ErrorModal.show();
     }
     else{
         userName.classList.remove("callback-form-input-error");
     }
 
-    if(!userEmail.value.trim()||!isEmailValid(userEmail.value)){
+    if(!userEmail.value.trim()||!isEmailValid(userEmail.value)||userEmail.value==="email@example.com"){
         userEmail.classList.add("callback-form-input-error");
         hasError = true;
+        ErrorModal.show();
     }
     else{
         userEmail.classList.remove("callback-form-input-error");
@@ -46,6 +61,7 @@ callbackFrom.addEventListener("submit", function(event){
     if(!userPhone.value.trim()||!isPhoneValid(userPhone.value)){
         userPhone.classList.add("callback-form-input-error");
         hasError = true;
+        ErrorModal.show();
     }
     else{
         userPhone.classList.remove("callback-form-input-error");
@@ -54,8 +70,8 @@ callbackFrom.addEventListener("submit", function(event){
     if(hasError)
     {
         return;
-    }*/
-    
+    }
+    ModalSuccess.show();
 
     // send to google-docs
     var http = new XMLHttpRequest();
